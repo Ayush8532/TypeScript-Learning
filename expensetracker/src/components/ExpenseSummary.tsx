a4a637822e71581293372b8e7bc20bce328ae120
Ayush8532 <uniyalayush8532@gmail.com>
2025-03-18 00:24:38 +0530

    added expense tracker monthly expenses in sorted form

--- expensetracker/src/components/ExpenseSummary.tsx
+++ expensetracker/src/components/ExpenseSummary.tsx
@@ -9,23 +9,36 @@ const ExpenseSummary:React.FC<Props>=({expenses})=>{
     const monthlyExpenses:{[key:string]:number}={};
 
     expenses.forEach((expenses)=>{
-        const month=new Date(expenses.date).toLocaleString("default",{month:"long",year:"numeric"});
-        if(!monthlyExpenses[month]){
-            monthlyExpenses[month]=0;
+        const date=new Date(expenses.date)
+        const monthKey=`${date.getFullYear()}-${date.getMonth()}`;
+      
+        if(!monthlyExpenses[monthKey]){
+            monthlyExpenses[monthKey]=0;
         }
-        monthlyExpenses[month]+=expenses.amount;
+        monthlyExpenses[monthKey]+=expenses.amount;
+    });
+    const sortedMonths=Object.keys(monthlyExpenses).sort((a,b)=>{
+        const[yearA,monthA]=a.split("-").map(Number);
+        const[yearB,monthB]=b.split("-").map(Number);
+
+        return yearA-yearB||monthA-monthB;
     })
+    console.log(sortedMonths);
     return(
         <>
-        <div>
+  <div>
             <h2>Monthly Expenses</h2>
             <ul>
-                {Object.entries(monthlyExpenses).map(([month,total])=>
-                <li key={month}>
-                    <strong>{month}</strong>"${total}"
-                </li>
-                )}
+                {sortedMonths.map((monthKey) => {
+                    const [year, month] = monthKey.split("-").map(Number);
+                    const monthName = new Date(year, month).toLocaleString("default", { month: "long", year: "numeric" });
 
+                    return (
+                        <li key={monthKey}>
+                            <strong>{monthName}</strong>: "${monthlyExpenses[monthKey]}"
+                        </li>
+                    );
+                })}
             </ul>
         </div>
         </>
